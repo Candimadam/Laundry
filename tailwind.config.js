188af18f99459b5da9e47c3dddb0abce1e7f940e
Candimadam <candiadam23@gmail.com>
2024-04-03 10:21:25 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html", "login.html", "dashboard.html"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#14b8a6",
+        secondary: "#64748b",
+        dark: "#0f172a",
+      },
+      screens: {
+        "2xl": "1320px",
+      },
+    },
+  },
+  plugins: [],
+};
