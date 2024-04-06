fa6f0ff64b9ece7141cec0f38151b10b440ab1b1
Candimadam <candiadam23@gmail.com>
2024-04-06 14:10:14 +0700

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    modified:   dashboard.html
    modified:   login.html
    new file:   register.html
    modified:   src/output.css
    modified:   tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["index.html", "login.html", "dashboard.html"],
+  content: ["index.html", "login.html", "register.html", "dashboard.html"],
   theme: {
     extend: {
       colors: {
